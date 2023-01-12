9f941e022c5c62b48b55729619e58931e2d28fea
rahardian-dwi-saputra <radiandwi2206@gmail.com>
2023-01-12 14:33:37 +0700

    Menambahkan file template dan membuat form login

---
+++ app/Http/Controllers/Backend/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller{
+    
+    public function index(){
+    	return view('backend.login'); 
+    }
+    public function authenticate(Request $request){
+    	
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', [AuthController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'authenticate']);
